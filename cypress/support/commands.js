8c15d85884c32af9a342e4c5df7d1bf6fe4f8722
oleksandr-khoma <d3m11av@gmail.com>
2023-12-08 10:50:51 +0200

    feat: api requests added

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -42,3 +42,71 @@ Cypress.Commands.add("login", (email, password) => {
   cy.contains("button", "Log In").click();
   cy.url().should("contain", "pages");
 });
+
+Cypress.Commands.add("getPostById", (postId) => {
+  return cy
+    .request({
+      method: "GET",
+      url: `https://jsonplaceholder.typicode.com/posts/${postId}`,
+    })
+    .should((response) => {
+      expect(response.status).to.equal(200);
+    })
+    .its("body");
+});
+
+Cypress.Commands.add("getPosts", () => {
+  return cy
+    .request({
+      method: "GET",
+      url: `https://jsonplaceholder.typicode.com/posts`,
+    })
+    .should((response) => {
+      expect(response.status).to.equal(200);
+    })
+    .its("body");
+});
+
+Cypress.Commands.add("createNewPost", (postData) => {
+  return cy
+    .request({
+      method: "POST",
+      url: `https://jsonplaceholder.typicode.com/posts`,
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(postData),
+    })
+    .should((response) => {
+      expect(response.status).to.equal(201);
+    })
+    .its("body");
+});
+
+Cypress.Commands.add("updatePost", (postId, postData) => {
+  return cy
+    .request({
+      method: "PUT",
+      url: `https://jsonplaceholder.typicode.com/posts/${postId}`,
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(postData),
+    })
+    .should((response) => {
+      expect(response.status).to.equal(200);
+    })
+    .its("body");
+});
+
+Cypress.Commands.add("deletePost", (postId) => {
+  return cy
+    .request({
+      method: "DELETE",
+      url: `https://jsonplaceholder.typicode.com/posts/${postId}`,
+    })
+    .should((response) => {
+      expect(response.status).to.equal(200);
+    })
+    .its("body");
+});
